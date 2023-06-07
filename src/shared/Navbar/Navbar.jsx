67442bc741f496b3bab9207775e6db47766fb589
monsur776e <monsuralam8897@gmail.com>
2023-06-07 20:29:29 +0600

    navbar added

--- src/shared/Navbar/Navbar.jsx
+++ src/shared/Navbar/Navbar.jsx
@@ -1,97 +1,76 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { FaUser, FaSignInAlt } from "react-icons/fa";
 
-const Navbar = ({ user }) => {
+const Navbar = () => {
    return (
-      <nav className="bg-gray-800 py-4 px-8 sm:px-16">
-         <div className="flex items-center justify-between">
-            <Link to="/" className="text-white text-lg font-bold">
-               Shutterschool
-            </Link>
-            <div className="flex items-center">
-               <input type="checkbox" id="menu-toggle" className="hidden" />
-               <label
-                  htmlFor="menu-toggle"
-                  className="cursor-pointer md:hidden flex items-center">
+      <div className="navbar bg-base-100">
+         <div className="navbar-start">
+            <div className="dropdown">
+               <label tabIndex={0} className="btn btn-ghost lg:hidden">
                   <svg
-                     className="fill-current text-white w-6 h-6 mr-2"
                      xmlns="http://www.w3.org/2000/svg"
-                     viewBox="0 0 24 24">
+                     className="h-5 w-5"
+                     fill="none"
+                     viewBox="0 0 24 24"
+                     stroke="currentColor">
                      <path
-                        fillRule="evenodd"
-                        d="M3 4h18a1 1 0 0 1 0 2H3a1 1 0 0 1 0-2zm0 7h18a1 1 0 0 1 0 2H3a1 1 0 0 1 0-2zm0 7h18a1 1 0 0 1 0 2H3a1 1 0 0 1 0-2z"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M4 6h16M4 12h8m-8 6h16"
                      />
                   </svg>
                </label>
-               <div className="hidden md:flex items-center">
-                  <Link
-                     to="/instructors"
-                     className="text-white hover:text-gray-300 mx-2">
-                     Instructors
-                  </Link>
-                  <Link
-                     to="/classes"
-                     className="text-white hover:text-gray-300 mx-2">
-                     Classes
-                  </Link>
-                  {user ? (
-                     <Link
-                        to="/dashboard"
-                        className="text-white hover:text-gray-300">
-                        <img
-                           src={user.profilePicture}
-                           alt="User Profile"
-                           className="w-8 h-8 rounded-full"
-                        />
-                     </Link>
-                  ) : (
-                     <Link
-                        to="/login"
-                        className="text-white hover:text-gray-300 flex items-center">
-                        <FaSignInAlt className="mr-1" />
-                        Login
-                     </Link>
-                  )}
-               </div>
+               <ul
+                  tabIndex={0}
+                  className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                  <li>
+                     <a>Item 1</a>
+                  </li>
+                  <li>
+                     <a>Parent</a>
+                     <ul className="p-2">
+                        <li>
+                           <a>Submenu 1</a>
+                        </li>
+                        <li>
+                           <a>Submenu 2</a>
+                        </li>
+                     </ul>
+                  </li>
+                  <li>
+                     <a>Item 3</a>
+                  </li>
+               </ul>
             </div>
+            <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
          </div>
-         <div className="hidden md:hidden">
-            <input type="checkbox" id="menu-toggle" className="hidden" />
-            <div className="w-full bg-gray-800 pt-2">
-               <div className="flex items-center justify-between">
-                  <Link
-                     to="/instructors"
-                     className="block py-2 px-4 text-white hover:bg-gray-700">
-                     Instructors
-                  </Link>
-                  <Link
-                     to="/classes"
-                     className="block py-2 px-4 text-white hover:bg-gray-700">
-                     Classes
-                  </Link>
-                  {user ? (
-                     <Link
-                        to="/dashboard"
-                        className="block py-2 px-4 text-white hover:bg-gray-700">
-                        <img
-                           src={user.profilePicture}
-                           alt="User Profile"
-                           className="w-8 h-8 rounded-full"
-                        />
-                     </Link>
-                  ) : (
-                     <Link
-                        to="/login"
-                        className="block py-2 px-4 text-white hover:bg-gray-700">
-                        <FaSignInAlt className="mr-1" />
-                        Login
-                     </Link>
-                  )}
-               </div>
-            </div>
+         <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">
+               <li>
+                  <a>Item 1</a>
+               </li>
+               <li tabIndex={0}>
+                  <details>
+                     <summary>Parent</summary>
+                     <ul className="p-2">
+                        <li>
+                           <a>Submenu 1</a>
+                        </li>
+                        <li>
+                           <a>Submenu 2</a>
+                        </li>
+                     </ul>
+                  </details>
+               </li>
+               <li>
+                  <a>Item 3</a>
+               </li>
+            </ul>
          </div>
-      </nav>
+         <div className="navbar-end">
+            <a className="btn">Button</a>
+         </div>
+      </div>
    );
 };
 
